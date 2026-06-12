d1cfc804f688a0d36cd34807d60d7e44d651f139
Centaurovet <centaurovetbr@gmail.com>
2026-06-12 19:59:44 -0300

    Novo ícone EquiVet + apple-touch-icon (iOS) + SW v4

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "equivet-v3";
+const CACHE = "equivet-v4";
 const FILES = [
   "./",
   "./index.html",
@@ -6,7 +6,10 @@ const FILES = [
   "./equivet-uti.html",
   "./manifest-clinica.json",
   "./manifest-uti.json",
-  "./icon-clinica.svg",
+  "./icon-equivet.svg",
+  "./icon-512.png",
+  "./icon-192.png",
+  "./icon-180.png",
   "https://cdnjs.cloudflare.com/ajax/libs/react/18.2.0/umd/react.production.min.js",
   "https://cdnjs.cloudflare.com/ajax/libs/react-dom/18.2.0/umd/react-dom.production.min.js",
   "https://cdnjs.cloudflare.com/ajax/libs/babel-standalone/7.23.2/babel.min.js",
